2cb46e6b071657f6a53bf5fa4cb0d1295eb5b6c3
LahiruPethum <68985843+LahiruPethum@users.noreply.github.com>
2020-09-08 22:24:56 +0530

    Added Asset folder

---
+++ Assets/Scripts/Gamecont.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gamecont : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pausePanal;
+
+    private void Awake()
+    {
+        pausePanal.SetActive(false);
+    }
+    public void pauseGame() {
+        Time.timeScale = 0f;
+        pausePanal.SetActive(true);
+    }
+
+    public void resumeGame() {
+        Time.timeScale = 1f;
+        pausePanal.SetActive(false);
+    }
+
+    public void SceneLoader(int SceneIndex)
+
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
